d050835728e0ba1154710539ac457d6ecf99ca08
Daniel <dpalencia2112@gmail.com>
2021-02-22 10:34:50 -0800

    Tweaking HTML tags

--- hide-notification.js
+++ hide-notification.js
@@ -1,3 +1,5 @@
+
+// Hide the warning on the "Where to Look" page.
 document.onload
 let notice = document.getElementById("orion-visible-notice");
 let btn = document.getElementById("orion-visible-btn");
